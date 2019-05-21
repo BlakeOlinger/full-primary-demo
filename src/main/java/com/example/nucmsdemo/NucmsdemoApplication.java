2f9d6bc2f87ebb2917e864771c69c10f4dda87c5
blake <finalyetifive@gmail.com>
2019-05-21 12:24:52 -0400

    init commit

---
+++ src/main/java/com/example/nucmsdemo/NucmsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.nucmsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NucmsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NucmsdemoApplication.class, args);
+    }
+
+}
